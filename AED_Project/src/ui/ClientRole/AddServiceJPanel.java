e83a3af6efc3ee7db2d4733ff718a74b66a6d3b3
HimanshuT <talele.h@northeastern.edu>
2022-12-11 05:10:25 -0500

    Client Add Service Pannel upgrade

--- AED_Project/src/ui/ClientRole/AddServiceJPanel.java
+++ AED_Project/src/ui/ClientRole/AddServiceJPanel.java
@@ -1,15 +1,18 @@
 package ui.ClientRole;
+/**
+ *
+ * @author prathmeshw
+ */
 
-import Model.Admin;
-import Model.HallBooking;
+import Model.*;
 import java.util.function.Consumer;
 
 
 public class AddServiceJPanel extends javax.swing.JPanel {
-    
+
     private Admin systems;
     private Consumer<HallBooking> callOnEventBooking;
-    private Consumer<HallBooking> callOnAdminBooking;
+    private Consumer<HallBooking> callOnCateringBooking;
     private Consumer<HallBooking> callOnEntertainmentBooking;
     private Consumer<HallBooking> callOnResortServiceBooking;
     private Runnable backButton4;
@@ -17,33 +20,19 @@ public class AddServiceJPanel extends javax.swing.JPanel {
     private String hotelName;
     private HallBooking booking;
 
-
     public AddServiceJPanel(Admin systems, Consumer<HallBooking> callOnEventBooking, Consumer<HallBooking> callOnOrderBooking,
             Consumer<HallBooking> callOnEntertainmentBooking, Consumer<HallBooking> callOnResortServiceBooking, Runnable backButton, String username, HallBooking booking) {
-        
         initComponents();
         this.systems = systems;
         this.callOnEventBooking = callOnEventBooking;
-        this.callOnAdminBooking = callOnOrderBooking;
+        this.callOnCateringBooking = callOnOrderBooking;
         this.callOnEntertainmentBooking = callOnEntertainmentBooking;
         this.callOnResortServiceBooking = callOnResortServiceBooking;
         this.backButton4 = backButton;
         this.username = username;
         this.booking = booking;
-//        setBackground(new java.awt.Color(255, 208, 56));
-//        backBtn.setBackground(new java.awt.Color(0, 102, 102));
-//        backBtn.setOpaque(true);
-//        btnEvent.setBackground(new java.awt.Color(0, 102, 102));
-//        btnEvent.setOpaque(true);
-//        btnAdmin.setBackground(new java.awt.Color(0, 102, 102));
-//        btnAdmin.setOpaque(true);
-//        btnEntertainment.setBackground(new java.awt.Color(0, 102, 102));
-//        btnEntertainment.setOpaque(true);
-//        btnOthers.setBackground(new java.awt.Color(0, 102, 102));
-//        btnOthers.setOpaque(true);
     }
 
-    
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -51,17 +40,21 @@ public class AddServiceJPanel extends javax.swing.JPanel {
         jLabel1 = new javax.swing.JLabel();
         backBtn = new javax.swing.JButton();
         btnEvent = new javax.swing.JButton();
+        btnCatering = new javax.swing.JButton();
         btnEntertainment = new javax.swing.JButton();
         btnOthers = new javax.swing.JButton();
-        btnCatering = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
 
+        setBackground(new java.awt.Color(255, 255, 255));
         setLayout(null);
 
         jLabel1.setFont(new java.awt.Font("Lucida Grande", 1, 36)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("VIEW SERVICES DETAILS");
         add(jLabel1);
-        jLabel1.setBounds(167, 137, 483, 43);
+        jLabel1.setBounds(180, 100, 483, 43);
 
+        backBtn.setBackground(new java.awt.Color(204, 204, 255));
         backBtn.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
         backBtn.setText("BACK");
         backBtn.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -73,6 +66,7 @@ public class AddServiceJPanel extends javax.swing.JPanel {
         add(backBtn);
         backBtn.setBounds(38, 27, 54, 36);
 
+        btnEvent.setBackground(new java.awt.Color(204, 204, 255));
         btnEvent.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
         btnEvent.setText("EVENT");
         btnEvent.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -82,30 +76,9 @@ public class AddServiceJPanel extends javax.swing.JPanel {
             }
         });
         add(btnEvent);
-        btnEvent.setBounds(132, 228, 262, 38);
+        btnEvent.setBounds(62, 438, 262, 38);
 
-        btnEntertainment.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
-        btnEntertainment.setText("ENTERTAINMENT");
-        btnEntertainment.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        btnEntertainment.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEntertainmentActionPerformed(evt);
-            }
-        });
-        add(btnEntertainment);
-        btnEntertainment.setBounds(132, 306, 262, 42);
-
-        btnOthers.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
-        btnOthers.setText("OTHERS");
-        btnOthers.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
-        btnOthers.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnOthersActionPerformed(evt);
-            }
-        });
-        add(btnOthers);
-        btnOthers.setBounds(412, 306, 262, 42);
-
+        btnCatering.setBackground(new java.awt.Color(204, 204, 255));
         btnCatering.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
         btnCatering.setText("CATERING");
         btnCatering.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
@@ -115,16 +88,45 @@ public class AddServiceJPanel extends javax.swing.JPanel {
             }
         });
         add(btnCatering);
-        btnCatering.setBounds(410, 230, 260, 40);
+        btnCatering.setBounds(444, 438, 262, 42);
+
+        btnEntertainment.setBackground(new java.awt.Color(204, 204, 255));
+        btnEntertainment.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        btnEntertainment.setText("ENTERTAINMENT");
+        btnEntertainment.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnEntertainment.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEntertainmentActionPerformed(evt);
+            }
+        });
+        add(btnEntertainment);
+        btnEntertainment.setBounds(62, 524, 262, 42);
+
+        btnOthers.setBackground(new java.awt.Color(204, 204, 255));
+        btnOthers.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+        btnOthers.setText("OTHERS");
+        btnOthers.setBorder(javax.swing.BorderFactory.createBevelBorder(javax.swing.border.BevelBorder.RAISED));
+        btnOthers.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOthersActionPerformed(evt);
+            }
+        });
+        add(btnOthers);
+        btnOthers.setBounds(444, 524, 262, 42);
+
+        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/ui/ClientRole/service.png"))); // NOI18N
+        jLabel2.setText("jLabel2");
+        add(jLabel2);
+        jLabel2.setBounds(0, 0, 840, 690);
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnEventActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEventActionPerformed
         callOnEventBooking.accept(booking);
     }//GEN-LAST:event_btnEventActionPerformed
 
-    private void btnAdminActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAdminActionPerformed
-        callOnAdminBooking.accept(booking);
-    }//GEN-LAST:event_btnAdminActionPerformed
+    private void btnCateringActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCateringActionPerformed
+        callOnCateringBooking.accept(booking);
+    }//GEN-LAST:event_btnCateringActionPerformed
 
     private void btnEntertainmentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEntertainmentActionPerformed
         callOnEntertainmentBooking.accept(booking);
@@ -138,10 +140,6 @@ public class AddServiceJPanel extends javax.swing.JPanel {
         callOnResortServiceBooking.accept(booking);
     }//GEN-LAST:event_btnOthersActionPerformed
 
-    private void btnCateringActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnCateringActionPerformed
-        callOnAdminBooking.accept(booking);
-    }//GEN-LAST:event_btnCateringActionPerformed
-
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton backBtn;
@@ -150,5 +148,6 @@ public class AddServiceJPanel extends javax.swing.JPanel {
     private javax.swing.JButton btnEvent;
     private javax.swing.JButton btnOthers;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     // End of variables declaration//GEN-END:variables
 }
